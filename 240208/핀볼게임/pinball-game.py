982d8c172c72ed0d58ce58e7ab1027410730eca4
코코종 <61327153+kokojong@users.noreply.github.com>
2024-02-08 21:14:53 +0900

    Title: 핀볼게임, Time: 138ms, Memory: 25MB, Status: Passed - CodeTree

--- 240208/핀볼게임/pinball-game.py
+++ 240208/핀볼게임/pinball-game.py
@@ -17,35 +17,37 @@ def simulate(r, c, d): # r, c 에서 시작하며 방향이 d
     cnt = 1
 
     while True:
-        rr = r + dr[d]
-        cc = c + dc[d]
+        if 0<=r<N and 0<=c<N:
+            if board[r][c] == 1:
+                if d == 0:
+                    d = 1
+                elif d == 1:
+                    d = 0
+                elif d == 2:
+                    d = 3
+                elif d == 3:
+                    d = 2
+            elif board[r][c] == 2:
+                if d == 0:
+                    d = 3
+                elif d == 1:
+                    d = 2
+                elif d == 2:
+                    d = 1
+                elif d == 3:
+                    d = 0
 
-        if 0<=rr<N and 0<=cc<N:
+            rr = r + dr[d]
+            cc = c + dc[d]
+
             # print("r, c", r, c, rr, cc)
-            if board[rr][cc] == 1:
-                if d == 0:
-                    d = 1
-                elif d == 1:
-                    d = 0
-                elif d == 2:
-                    d = 3
-                elif d == 3:
-                    d = 2
-            elif board[rr][cc] == 2:
-                if d == 0:
-                    d = 3
-                elif d == 1:
-                    d = 2
-                elif d == 2:
-                    d = 1
-                elif d == 3:
-                    d = 0
+
             r = rr
             c = cc
             cnt += 1
         else:
-            answer = max(answer, cnt + 1)
-            return cnt+1 # 밖으로 나간거라면 cnt return
+            answer = max(answer, cnt)
+            return cnt # 밖으로 나간거라면 cnt return
 
 
 for i in range(N):
@@ -54,6 +56,6 @@ for i in range(N):
     simulate(0, i, 2)
     simulate(i, N-1, 3)
     
-# print(simulate(0, 2, 2))
+# print(simulate(1, 2, 3))
 
 print(answer)

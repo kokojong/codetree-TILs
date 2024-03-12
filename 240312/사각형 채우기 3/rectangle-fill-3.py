ea2ddcd1f03588ebeea85443879fbb72b0e7bf81
코코종 <61327153+kokojong@users.noreply.github.com>
2024-03-12 23:08:33 +0900

    Title: 사각형 채우기 3, Time: 110ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240312/사각형 채우기 3/rectangle-fill-3.py
+++ 240312/사각형 채우기 3/rectangle-fill-3.py
@@ -12,7 +12,7 @@ if n == 2:
     exit()
 
 dp[3] = dp[1]*3 + dp[2]*2 + dp[0] * 2
-for i in range(3, n):
+for i in range(3, n+1):
     dp[i] = dp[i-1] * 3 + dp[i-2] *2 + dp[i-3] * 2
     dp[i] %= 1_000_000_007
 

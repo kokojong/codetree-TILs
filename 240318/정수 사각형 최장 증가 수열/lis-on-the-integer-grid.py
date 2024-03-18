6403c9b484ad1bae47f41b844ae37cee199fd0df
코코종 <61327153+kokojong@users.noreply.github.com>
2024-03-18 21:10:29 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 287ms, Memory: 28MB, Status: Wrong Answer - CodeTree

---
+++ 240318/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -0,0 +1,40 @@
+N = int(input())
+
+board = []
+for _ in range(N):
+    row = list(map(int, input().split()))
+    board.append(row)
+
+dp = [[1 for _ in range(N)] for _ in range(N)] # 이 지점부터 이동이 가능한 최대 거리
+
+dr = [-1, 0, 1 ,0]
+dc = [0, -1, 0, -1]
+
+
+for r in range(N):
+    for c in range(N):
+        
+        for i in range(4):
+            rr = r + dr[i]
+            cc = c + dc[i]
+
+            if 0<=rr<N and 0<=cc<N and board[r][c] < board[rr][cc]:
+                dp[rr][cc] = max(dp[r][c] + 1, dp[rr][cc])
+
+for r in range(N):
+    for c in range(N):
+        
+        for i in range(4):
+            rr = r + dr[i]
+            cc = c + dc[i]
+
+            if 0<=rr<N and 0<=cc<N and board[r][c] < board[rr][cc]:
+                dp[rr][cc] = max(dp[r][c] + 1, dp[rr][cc])
+
+answer = 0
+for d in dp:
+    answer = max(max(d), answer)
+print(answer)
+
+# for i in range(N):
+#     for j in range(N):

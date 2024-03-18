f5a9a36e02ec0e0c03216c639147888438230cf2
코코종 <61327153+kokojong@users.noreply.github.com>
2024-03-18 22:37:33 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 123ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240318/정수 사각형 최댓값의 최소/minimax-path-in-square.py
+++ 240318/정수 사각형 최댓값의 최소/minimax-path-in-square.py
@@ -5,7 +5,7 @@ for _ in range(N):
     row = list(map(int, input().split()))
     board.append(row)
 
-dp = [[0 for _ in range(N)] for _ in range(N)]
+dp = [[1000000 for _ in range(N)] for _ in range(N)]
 
 # 오른쪽 혹은 밑으로만
 # 거쳐간 위치에 적혀있는 숫자들 중 최댓값을 최소로 하기
@@ -22,11 +22,32 @@ for r in range(1, N):
 
 for r in range(1, N):
     for c in range(1, N):
-        if board[r][c] >= max(dp[r][c-1], dp[r-1][c]):
+        up = dp[r-1][c]
+        left = dp[r][c-1]
+
+        # print("r, c, dp", r, c, dp)
+        
+        if board[r][c] > up and board[r][c] > left:
             dp[r][c] = board[r][c]
-        else:
+        
+        if board[r][c] <= up:
+            dp[r][c] = min(up, dp[r][c])
+        
+        if board[r][c] <= left:
+            dp[r][c] = min(left, up, dp[r][c])
+
+        # if board[r][c] >= max(dp[r][c-1], dp[r-1][c]):
+        #     dp[r][c] = board[r][c]
+        # else:
         # dp[r][c] = max(dp[r][c-1], dp[r-1][c], board[r][c])
-            dp[r][c] = min(dp[r][c-1], dp[r-1][c]) # 가장 작은 수로 경로 최신화
+        #    dp[r][c] = min(dp[r][c-1], dp[r-1][c]) # 가장 작은 수로 경로 최신화
+
+        # print("결과", dp)
 
 # print(dp)
 print(dp[-1][-1])
+
+# 3
+# 1 1 3
+# 6 1 1
+# 10 10 2

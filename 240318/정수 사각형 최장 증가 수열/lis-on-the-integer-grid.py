3df6dc70a1417cd0461422b4a9daac825c779b56
코코종 <61327153+kokojong@users.noreply.github.com>
2024-03-18 21:18:28 +0900

    Title: 정수 사각형 최장 증가 수열, Time: 237ms, Memory: 28MB, Status: Wrong Answer - CodeTree

--- 240318/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
+++ 240318/정수 사각형 최장 증가 수열/lis-on-the-integer-grid.py
@@ -10,22 +10,26 @@ dp = [[1 for _ in range(N)] for _ in range(N)] # 이 지점부터 이동이 가�
 dr = [-1, 0, 1 ,0]
 dc = [0, 1, 0, -1]
 
-cells = []
+
 for r in range(N):
     for c in range(N):
-        cells.append((board[r][c], r, c))
+        
+        for i in range(4):
+            rr = r + dr[i]
+            cc = c + dc[i]
 
-cells.sort()
+            if 0<=rr<N and 0<=cc<N and board[r][c] < board[rr][cc]:
+                dp[rr][cc] = max(dp[r][c] + 1, dp[rr][cc])
 
-# print(cells)
+for r in range(N):
+    for c in range(N):
+        
+        for i in range(4):
+            rr = r + dr[i]
+            cc = c + dc[i]
 
-for k, r, c in cells:
-    for i in range(4):
-        rr = r + dr[i]
-        cc = c + dc[i]
-
-        if 0<=rr<N and 0<=cc<N and board[r][c] < board[rr][cc]:
-            dp[rr][cc] = max(dp[r][c] + 1, dp[rr][cc])
+            if 0<=rr<N and 0<=cc<N and board[r][c] < board[rr][cc]:
+                dp[rr][cc] = max(dp[r][c] + 1, dp[rr][cc])
 
 answer = 0
 for d in dp:

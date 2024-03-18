3f1db029531a7137e96274a31f118d1fac002c60
코코종 <61327153+kokojong@users.noreply.github.com>
2024-03-18 22:03:00 +0900

    Title: 정수 사각형 최댓값의 최소, Time: 99ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240318/정수 사각형 최댓값의 최소/minimax-path-in-square.py
@@ -0,0 +1,31 @@
+N = int(input())
+
+board = []
+for _ in range(N):
+    row = list(map(int, input().split()))
+    board.append(row)
+
+dp = [[0 for _ in range(N)] for _ in range(N)]
+
+# 오른쪽 혹은 밑으로만
+# 거쳐간 위치에 적혀있는 숫자들 중 최댓값을 최소로 하기
+
+dp[0][0] = board[0][0]
+
+for c in range(1, N):
+    dp[0][c] = max(dp[0][c-1], board[0][c])
+
+for r in range(1, N):
+    dp[r][0] = max(dp[r-1][0], board[r][0])
+
+
+for r in range(1, N):
+    for c in range(1, N):
+        if board[r][c] >= max(dp[r][c-1], dp[r-1][c]):
+            dp[r][c] = board[r][c]
+        else:
+        # dp[r][c] = max(dp[r][c-1], dp[r-1][c], board[r][c])
+            dp[r][c] = min(dp[r][c-1], dp[r-1][c])
+
+# print(dp)
+print(dp[-1])

05038b7b20aae544f2c18624ec441b3e9b3f11cb
코코종 <61327153+kokojong@users.noreply.github.com>
2024-03-19 20:16:59 +0900

    Title: 최대 점프 횟수, Time: 138ms, Memory: 25MB, Status: Wrong Answer - CodeTree

--- 240319/최대 점프 횟수/maximum-number-of-jumps.py
+++ 240319/최대 점프 횟수/maximum-number-of-jumps.py
@@ -4,24 +4,15 @@ arr = list(map(int, input().split()))
 
 # 최대 점프 가능 거리
 
-dp = [0 for _ in range(N)]
+dp = [-1 for _ in range(N)]
 
 # i에서부터 뒤로 가면서 하나씩 살펴보고 +1 되는지 확인하기??
 
+dp[0] = 0
+
 for i in range(N):
-    k = arr[i] # 최대 k만큼 점프할 수 있음
+    for j in range(i):
+        if j + arr[j] >= i:
+            dp[i] = max(dp[j]+1, dp[i])
 
-    for j in range(1,k+1):
-        if i+j < N:
-            dp[i+j] += 1
-    
-answer = 0
-# print(dp)
-for i in range(1, N):
-    answer = max(dp[i], answer)
-    if dp[i] == 0:
-        break
-    # else:
-    #     answer = max(dp[i], answer)
-
-print(answer)
+print(max(dp))

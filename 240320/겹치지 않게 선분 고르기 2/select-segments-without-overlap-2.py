57a04484d32d1fbbcbd8c0db2dab2e745236a06e
코코종 <61327153+kokojong@users.noreply.github.com>
2024-03-20 15:40:16 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 140ms, Memory: 25MB, Status: Passed - CodeTree

--- 240320/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
+++ 240320/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.py
@@ -5,16 +5,27 @@ for _ in range(N):
     x1, x2 = map(int, input().split())
     arr.append((x1, x2))
 
+arr.sort(key = lambda x: (x[1], x[1]-x[0])) # 끝나는 점이 더 작고, 길이가 짧은 순으로 나열
 # print(arr)
-arr.sort()
 
-dp = [0 for _ in range(1001)] # 최대 위치가 1000
-# dp[i]로 끝나는 선분?
+last = 0
+answer = 0
 
 for a, b in arr:
-    k = max(dp[:a])
-    
-    for i in range(a, b+1):
-        dp[i] = max(k+1, dp[i])
+    if a > last:
+        last = b
+        answer += 1
 
-print(max(dp))
+print(answer)
+
+# dp = [0 for _ in range(1001)] # 최대 위치가 1000
+# # dp[i]로 끝나는 선분?
+
+# for a, b in arr:
+#     k = max(dp[:a])
+    
+#     for i in range(a, b+1):
+#         dp[i] = max(k+1, dp[i])
+
+# print(dp)
+# print(max(dp))

d94284dc28068e11feeff0e0fda08cf881b61d92
코코종 <61327153+kokojong@users.noreply.github.com>
2024-03-28 01:09:09 +0900

    Title: 알바로 부자 되기, Time: 108ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 240328/알바로 부자 되기/being-rich-by-working-part-time.py
+++ 240328/알바로 부자 되기/being-rich-by-working-part-time.py
@@ -14,7 +14,7 @@ for i in range(1, N):
     s,e,p = map(int, arr[i])
 
     for j in range(i):
-        if j > e:
+        if arr[j][1] > e:
             dp[i] = dp[j] + p # 안겹침
         else:
             dp[i] = max(p, dp[i])

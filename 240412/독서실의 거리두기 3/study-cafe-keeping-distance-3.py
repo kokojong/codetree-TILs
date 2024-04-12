de3091ffa20540f134cfb31a548b40dc8fcfcf51
코코종 <61327153+kokojong@users.noreply.github.com>
2024-04-12 17:09:44 +0900

    Title: 독서실의 거리두기 3, Time: 103ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240412/독서실의 거리두기 3/study-cafe-keeping-distance-3.py
@@ -0,0 +1,12 @@
+N = int(input())
+
+arr = list(map(int, input()))
+
+last = 0
+dis = []
+for i in range(N):
+    if arr[i] == 1:
+        dis.append((i-last)//2)
+        last = i
+
+print(max(dis))
